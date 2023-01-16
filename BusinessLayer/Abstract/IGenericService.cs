bbc2abb42b241d259997f7ffe496aef6acfce362
furk4nk <furkankip@hotmail.com>
2023-01-16 12:45:47 +0300

    Add project files.

---
+++ BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IGenericService<T> where T : class
+    {
+        List<T> TGetList();
+        void TDelete(T t);
+        void TInsert(T t);
+        void TUpdate(T t);
+        T TGetById(int ID);
+
+    }
+}
---
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        private readonly IAboutDal _AboutDal;
+
+        public AboutManager(IAboutDal aboutDal)
+        {
+            _AboutDal = aboutDal;
+        }
+
+        public void TDelete(About t)
+        {
+            _AboutDal.Delete(t);
+        }
+
+        public About TGetById(int ID)
+        {
+            return _AboutDal.GetById(ID);
+        }
+
+        public List<About> TGetList()
+        {
+            return _AboutDal.GetList();
+        }
+
+        public void TInsert(About t)
+        {
+            _AboutDal.Insert(t);
+        }
+
+        public void TUpdate(About t)
+        {
+            _AboutDal.Update(t);
+        }
+    }
+}
